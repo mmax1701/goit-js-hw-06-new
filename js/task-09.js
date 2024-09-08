0d7177869425f2171ee895915d6e9dc5e0b22fa1
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-08 17:57:44 +0300

    js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector('.widget')
+const color = document.querySelector('.color')
+const button = document.querySelector('.change-color')
+
+button.addEventListener('click', onClick)
+
+function onClick() {
+  color.textContent = getRandomHexColor();
+  widget.parentNode.style.backgroundColor = color.textContent;
+  
+}
