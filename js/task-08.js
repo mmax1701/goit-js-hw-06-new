410469580afd64cb7a15ffc33384db2891c8377c
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-08 17:28:42 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const elem = evt.target.elements;
+    if (elem.email.value === "" || elem.password.value === "") {
+        alert('Заполни все поля!')
+        return;
+    }
+
+    console.log(`user = {email: ${elem.email.value}, password: ${elem.password.value}}`);
+    form.reset();
+}
